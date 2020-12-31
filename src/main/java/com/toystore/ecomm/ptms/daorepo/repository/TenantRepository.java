f83444cada8525afaf6461d840624c42e2de1967
PrvSunKS <48259084+PrvSunKS@users.noreply.github.com>
2021-01-01 02:54:21 +0530

    Added a Custom Query in order to Fetch Tenant by Verification ID

--- src/main/java/com/toystore/ecomm/ptms/daorepo/repository/TenantRepository.java
+++ src/main/java/com/toystore/ecomm/ptms/daorepo/repository/TenantRepository.java
@@ -19,7 +19,7 @@ public interface TenantRepository extends CrudRepository<TenantInfo, Integer>, T
 	List<TenantInfo> findByTenantUsername(String tenantUsername);
 	List<TenantInfo> findByTenantEmail(String tenantEmail);
 	List<TenantInfo> findByTenantVerified(String tenantVerified);
-	//List<TenantInfo> findByTenantVerificationCode(String tenantVerificationCode);
+	
 	
 	@Query("SELECT t FROM TenantInfo t WHERE LOWER(t.tenantName) = :tenantName AND LOWER(t.tenantVerified) = :tenantVerified")
 	List<TenantInfo> findByTenantVerified(@Param("tenantName") String tenantName,  @Param("tenantVerified") String tenantVerified);
@@ -56,6 +56,9 @@ public interface TenantRepository extends CrudRepository<TenantInfo, Integer>, T
 	
 	@Query("SELECT t FROM TenantInfo t WHERE LOWER(t.tenantUsername) = :tenantUsername AND LOWER(t.tenantName) LIKE %:tenantName% AND LOWER(t.tenantEmail) LIKE %:tenantEmail% AND LOWER(t.tenantVerified) = :tenantVerified")
 	List<TenantInfo> findByTenantNameEmailVerification(@Param("tenantUsername") String tenantUsername, @Param("tenantName") String tenantName, @Param("tenantEmail") String tenantEmail, @Param("tenantVerified") String tenantVerified);
+	
+	@Query("SELECT t FROM TenantInfo t WHERE LOWER(t.verificationId) LIKE %:verificationId%")
+	List<TenantInfo> findByVerificationId(String verificationId);
 	
 	@Modifying
 	@Query("UPDATE TenantInfo t SET t.tenantVerified = 'Y' WHERE LOWER(t.verificationId) LIKE %:verificationId%")
